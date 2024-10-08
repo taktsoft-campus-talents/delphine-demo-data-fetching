a94baa0484bc44c10eedcb75460de81761437e6a
Ralf Siewert <ralf@actyvy.st>
2024-10-08 17:14:31 +0200

    add promises demo

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,32 @@
 import { useState } from "react";
 import "./App.css";
 
+const users = [
+  {
+    name: "Ralf",
+  },
+  {
+    name: "Stacy",
+  },
+  {
+    name: "Ladan",
+  },
+];
+
+const simulateFetch = () => {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      if (Math.random() < 0.5) {
+        console.log("promise failed");
+        reject("Fehler");
+      } else {
+        console.log("promise resolved!");
+        resolve(users);
+      }
+    }, 5000);
+  });
+};
+
 function App() {
   const [users, setUsers] = useState([]);
   const [isLoading, setIsLoading] = useState(false);
@@ -27,6 +53,35 @@ function App() {
     }
   }
 
+  // async / await Schreibweise
+  async function handleSimulateFetch() {
+    try {
+      const promise = simulateFetch();
+      console.log("promise:", promise);
+      const data = await promise;
+      console.log("promise:", promise);
+    } catch (err) {
+      console.log(err);
+    }
+  }
+
+  function handleSimulateFetchThen() {
+    console.log("start");
+    simulateFetch()
+      .then((data) => {
+        console.log("This happens in success case");
+        console.log(data);
+      })
+      .catch((err) => {
+        console.log("This happens in error case");
+        console.log(err);
+      })
+      .finally(() => {
+        console.log("This happens in any case");
+      });
+    console.log("end");
+  }
+
   return (
     <>
       <h1>APIs and Data Fetching</h1>
@@ -38,6 +93,14 @@ function App() {
           return <li key={user.id}>{user.name}</li>;
         })}
       </ul>
+      <button onClick={handleSimulateFetchThen}>Call simulateFetch</button>
+      <button
+        onClick={() => {
+          console.log("I did something");
+        }}
+      >
+        Do something
+      </button>
     </>
   );
 }
